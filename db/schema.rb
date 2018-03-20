cc148aa897a1a452d5a643aa9e3a2450998ef8c6
Kent Crozier <kent.crozier@gmail.com>
2018-03-20 22:10:12 +0000

    Add one-to-many association between Product and Image models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320220355) do
+ActiveRecord::Schema.define(version: 20180320220907) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180320220355) do
     t.integer "productid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "product_id"
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
