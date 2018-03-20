aaa6ab06ef33bf8bb380708fbf216c51a22199e9
Kent Crozier <kent.crozier@gmail.com>
2018-03-20 21:25:58 +0000

    Create Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320212323) do
+ActiveRecord::Schema.define(version: 20180320212525) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "firstname"
