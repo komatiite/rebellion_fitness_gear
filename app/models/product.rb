9a7ac72828bbe7752c7e4c50efd057e77eeed301
Kent Crozier <kent.crozier@gmail.com>
2018-03-20 22:13:44 +0000

    Add one-to-many association between Category and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :orders
   has_many :images
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320220907) do
+ActiveRecord::Schema.define(version: 20180320221237) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 20180320220907) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "provinces", force: :cascade do |t|
