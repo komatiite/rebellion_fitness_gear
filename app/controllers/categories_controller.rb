7f8f1d47c5f8ac8fcc8fed77da74c23bf67ea450
Kent Crozier <kent.crozier@gmail.com>
2018-04-12 04:14:36 +0000

    Fix category menu bar and add pagination to 'All Products' index listing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
 
   def show
-    #@products = Product.order("name").page(params[:page]).per(5)
-    #@products = Product.where(:category => params[:id])
-    @products = Product.where(:category => params[:id]).page(params[:page]).per(5)
-    @category = Category.find(params[:id])
+    if params[:id] == '11'
+      redirect_to index_page_path
+    else
+      @products = Product.where(:category => params[:id]).page(params[:page]).per(5)
+      @category = Category.find(params[:id])
+    end
   end
 
 end
